afdeaa81ca2a627b2478506ffae4ac306baded09
Tung Pham <tung.42@gmail.com>
2024-07-10 17:46:27 +0700

    refactor code

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ function setEqualHeight(selector, offset = 0) {
     element.style.height = maxHeight + offset + "px";
   });
 }
+
 function limitText(selector, maxLength) {
-  const $element = $(selector);
-  $element.each(function () {
-    if ($(this).length) {
-      let text = $(this).text();
-      if (text.length > maxLength) {
-        text = text.substring(0, maxLength) + "...";
-        $(this).text(text);
-      }
+  const elements = $(selector);
+  elements.each(function () {
+    const text = $(this).text();
+    if (text.length > maxLength) {
+      $(this).text(text.substring(0, maxLength) + "...");
     }
   });
 }
+
 function stripHTMLTags(input) {
   return DOMPurify.sanitize(input, { ALLOWED_TAGS: [] });
 }
+
 const newsModal = document.getElementById("newsModal");
 const newsModalLabel = document.getElementById("newsModalLabel");
 const newsModalBody = document.getElementById("newsModalBody");
@@ -35,63 +35,53 @@ const newsModalLink = document.getElementById("newsModalLink");
 function fetchRss(rssUrl, containerID, newsName) {
   $.ajax({
     url: "fetch_rss.php",
-    data: {
-      rssUrl: rssUrl,
-    },
+    data: { rssUrl },
     dataType: "json",
     method: "GET",
     success: function (data) {
       const newsContainer = $(containerID);
       newsContainer.empty();
-      console.log(newsContainer);
+
       data.forEach((item) => {
-        var currentItem = item;
-        console.log(item);
         const descriptionWithoutImg = item.description.replace(
           /<img[^>]*>/g,
           ""
         );
         newsContainer.append(`
-                  <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-                    <div class="card" data-bs-toggle="modal" data-bs-target="#newsModal">
-                      <div class="card-body">
-                        <h5 class="card-title" title="${item.title}">${
-          item.title
-        }</h5>
-                        <p class="card-text">${stripHTMLTags(
-                          descriptionWithoutImg
-                        )}</p>
-                        <div class="card-description d-none">${
-                          item.description
-                        }</div>
-                        <span class="card-title-full d-none">${
-                          item.title
-                        }</span>
-                        <span class="card-link d-none">${item.link}</span>
-                        <small class="text-muted d-block my-3">${new Date(
-                          item.pubDate
-                        ).toLocaleDateString("vi-VN")}</small>
-                      </div>
-                    </div>
-                  </div>
-                `);
+          <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <div class="card" data-bs-toggle="modal" data-bs-target="#newsModal">
+              <div class="card-body">
+                <h5 class="card-title" title="${item.title}">${item.title}</h5>
+                <p class="card-text">${stripHTMLTags(descriptionWithoutImg)}</p>
+                <div class="card-description d-none">${item.description}</div>
+                <span class="card-title-full d-none">${item.title}</span>
+                <span class="card-link d-none">${item.link}</span>
+                <small class="text-muted d-block my-3">${new Date(
+                  item.pubDate
+                ).toLocaleDateString("vi-VN")}</small>
+              </div>
+            </div>
+          </div>
+        `);
       });
+
       limitText("h5.card-title", 50);
       limitText("p.card-text", 200);
     },
     error: function (err) {
-      console.log("Error fetching RSS feed:", err);
+      console.error("Error fetching RSS feed:", err);
     },
   });
 }
-newsModal.addEventListener("hide.bs.modal", (event) => {
+
+newsModal.addEventListener("hide.bs.modal", () => {
   newsModalLabel.textContent = "";
   newsModalBody.innerHTML = "";
   newsModalLink.setAttribute("href", "");
 });
+
 newsModal.addEventListener("show.bs.modal", (event) => {
   const relatedTarget = event.relatedTarget;
-  console.log(relatedTarget);
   newsModalLabel.textContent =
     relatedTarget.querySelector(".card-title-full").textContent;
   newsModalBody.innerHTML =
@@ -101,18 +91,21 @@ newsModal.addEventListener("show.bs.modal", (event) => {
     relatedTarget.querySelector(".card-link").textContent
   );
 });
+
 newsData.forEach((news) => {
-  fetchRss(news.url, "#" + news.name + "-news-container", news.name);
-  $("#" + news.name + "-tab").on("click", function () {
-    fetchRss(news.url, "#" + news.name + "-news-container", news.name);
+  fetchRss(news.url, `#${news.name}-news-container`, news.name);
+  $(`#${news.name}-tab`).on("click", () => {
+    fetchRss(news.url, `#${news.name}-news-container`, news.name);
   });
 });
+
 $(document).ready(function () {
   function updateHeights() {
     setEqualHeight("img.card-img-top");
     setEqualHeight("h5.card-title");
     setEqualHeight(".card-text");
   }
+
   function setEvents() {
     updateHeights();
     window.addEventListener("load", updateHeights);
@@ -120,18 +113,19 @@ $(document).ready(function () {
     $('button[data-bs-toggle="tab"]').on("shown.bs.tab", updateHeights);
     $(document).on("ajaxComplete", updateHeights);
   }
-  setEvents();
-  var btn = $("#back-to-top");
 
+  setEvents();
+
+  const topBtn = $("#back-to-top");
   $(window).scroll(function () {
     if ($(window).scrollTop() > 300) {
-      btn.fadeIn();
+      topBtn.fadeIn();
     } else {
-      btn.fadeOut();
+      topBtn.fadeOut();
     }
   });
 
-  btn.on("click", function (e) {
+  topBtn.on("click", function (e) {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "300");
   });
