efb1e4d7607aab2979e9d8942438658ebab54720
Tung Pham <tung.42@gmail.com>
2024-07-11 07:03:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,13 +27,15 @@ include './config.php';
     <!-- Custom CSS -->
     <link href="style.css?v=<?php echo date('Ymdhis'); ?>" rel="stylesheet">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3585118770961536" crossorigin="anonymous"></script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KTPXGLK');</script>
-    <!-- End Google Tag Manager -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L3WSNWL0N5"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-L3WSNWL0N5');
+    </script>
     <script>
         const newsData = <?php echo json_encode($newsData); ?>;
     </script>
